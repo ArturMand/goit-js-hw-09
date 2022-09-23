2dc3a106e84a186cfc155f429b62e7a1cc9476f8
ArturMand <108627874+ArturMand@users.noreply.github.com>
2022-09-23 14:00:29 +0300

    code refactoring task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,49 +2,39 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 /* Refs */
 const refs = {
-  delay: document.querySelector('input[name="delay"]'),
-  step: document.querySelector('input[name="step"]'),
-  amount: document.querySelector('input[name="amount"]'),
-  btnCreatePromise: document.querySelector('button[type="submit"]'),
+  form: document.querySelector('form'),
 };
-
-/* Variables */
-const { delay, step, amount, btnCreatePromise } = refs;
 
 /* Script */
 
-btnCreatePromise.addEventListener('click', e => {
-  startCreatePromise(e);
-});
+refs.form.addEventListener('submit', startCreatePromise);
 
 function startCreatePromise(e) {
   e.preventDefault();
-  const firstDelay = Number(delay.value);
+  const { delay, step, amount } = e.target.elements;
+  let firstDelay = Number(delay.value);
   const delayStep = Number(step.value);
-  const amountInputUser = amount.value;
-  for (let i = 0; i < amountInputUser; i += 1) {
-    createPromise(1 + i, firstDelay + i * delayStep)
-      .then(({ position, delay }) => onSuccess({ position, delay }))
-      .catch(({ position, delay }) => onError({ position, delay }));
+  const amountInputUser = Number(amount.value);
+  for (let i = 1; i <= amountInputUser; i += 1) {
+    createPromise(i, firstDelay)
+      .then(onSuccess)
+      .catch(onError);
+    firstDelay += delayStep;
   }
 }
 
 function createPromise(position, delay) {
-  const promise = new Promise((resolve, reject) => {
-    getPromiseFromBackend(position, delay, resolve, reject);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
-  return promise;
-}
-
-function getPromiseFromBackend(position, delay, resolve, reject) {
-  setTimeout(() => {
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      resolve({ position, delay });
-    } else {
-      reject({ position, delay });
-    }
-  }, delay);
+  refs.form.removeEventListener()
 }
 
 function onSuccess({ position, delay }) {
